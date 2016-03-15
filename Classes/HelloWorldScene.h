586b860cc1c463493763feeb3a4eedc1c356ce12
Jeff Wang <summer_insects@163.com>
2016-03-15 20:31:00 +0800

    修复编译警告

--- Classes/HelloWorldScene.h
+++ Classes/HelloWorldScene.h
@@ -7,7 +7,7 @@ class HelloWorld : public cocos2d::Layer {
 public:
     static cocos2d::Scene *createScene();
     virtual bool init() override;
-    void onKeyReleased(cocos2d::EventKeyboard::KeyCode keyCode, cocos2d::Event *unusedEvent);
+    virtual void onKeyReleased(cocos2d::EventKeyboard::KeyCode keyCode, cocos2d::Event *unusedEvent) override;
     CREATE_FUNC(HelloWorld);
 };
 
