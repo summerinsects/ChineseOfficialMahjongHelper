b53ab1ac796c1f395a9768bd79d63095a4855b1a
Jeff Wang <summer_insects@163.com>
2017-06-04 14:51:24 +0800

    no message

--- Classes/FanCalculator/FanCalculatorScene.cpp
+++ Classes/FanCalculator/FanCalculatorScene.cpp
@@ -77,6 +77,7 @@ bool FanCalculatorScene::init() {
         rt.getWinTile = std::bind(&TilePickWidget::getServingTile, tilePicker);
         rt.isStandingTilesContainsServingTile = std::bind(&TilePickWidget::isStandingTilesContainsServingTile, tilePicker);
         rt.countServingTileInFixedPacks = std::bind(&TilePickWidget::countServingTileInFixedPacks, tilePicker);
+        rt.isFixedPacksContainsKong = std::bind(&HandTilesWidget::isFixedPacksContainsKong, tilePicker);
         extraInfo->refreshByWinTile(rt);
     });
 
