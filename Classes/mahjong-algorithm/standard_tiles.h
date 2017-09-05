3c7757b8d18795469324ec5b25f502bcd0024e74
Jeff Wang <summer_insects@163.com>
2017-09-06 00:21:32 +0800

    新建一个文件用来放十三幺和组合龙数组

---
+++ Classes/mahjong-algorithm/standard_tiles.h
@@ -0,0 +1,53 @@
+﻿/****************************************************************************
+ Copyright (c) 2016-2017 Jeff Wang <summer_insects@163.com>
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy
+ of this software and associated documentation files (the "Software"), to deal
+ in the Software without restriction, including without limitation the rights
+ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+
+ The above copyright notice and this permission notice shall be included in all
+ copies or substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ SOFTWARE.
+ ****************************************************************************/
+
+#ifndef __MAHJONG_ALGORITHM__STANDARD_TILES_H__
+#define __MAHJONG_ALGORITHM__STANDARD_TILES_H__
+
+#include "tile.h"
+
+namespace mahjong {
+
+// 十三幺13面听
+static const tile_t standard_thirteen_orphans[13] = {
+    TILE_1m, TILE_9m, TILE_1s, TILE_9s, TILE_1p, TILE_9p, TILE_E, TILE_S, TILE_W, TILE_N, TILE_C, TILE_F, TILE_P
+};
+
+// 组合龙只有如下6种
+// 147m 258s 369p
+// 147m 369s 258p
+// 258m 147s 369p
+// 258m 369s 147p
+// 369m 147s 258p
+// 369m 258s 147p
+static const tile_t standard_knitted_straight[6][9] = {
+    { TILE_1m, TILE_4m, TILE_7m, TILE_2s, TILE_5s, TILE_8s, TILE_3p, TILE_6p, TILE_9p },
+    { TILE_1m, TILE_4m, TILE_7m, TILE_3s, TILE_6s, TILE_9s, TILE_2p, TILE_5p, TILE_8p },
+    { TILE_2m, TILE_5m, TILE_8m, TILE_1s, TILE_4s, TILE_7s, TILE_3p, TILE_6p, TILE_9p },
+    { TILE_2m, TILE_5m, TILE_8m, TILE_3s, TILE_6s, TILE_9s, TILE_1p, TILE_4p, TILE_7p },
+    { TILE_3m, TILE_6m, TILE_9m, TILE_1s, TILE_4s, TILE_7s, TILE_2p, TILE_5p, TILE_8p },
+    { TILE_3m, TILE_6m, TILE_9m, TILE_2s, TILE_5s, TILE_8s, TILE_1p, TILE_4p, TILE_7p },
+};
+
+}
+
+#endif
