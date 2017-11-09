57c2afad8ecc2a025d03de8c4e2bde24a7236250
Jeff Wang <summer_insects@163.com>
2017-11-09 21:22:12 +0800

    缩小RichText中牌图片的比例

--- Classes/FanTable/FanDefinitionScene.cpp
+++ Classes/FanTable/FanDefinitionScene.cpp
@@ -75,7 +75,7 @@ bool FanDefinitionScene::initWithIndex(size_t idx) {
             scale = maxWidth / TILE_WIDTH;
         }
 #else
-        float scale = 0.6f;
+        float scale = 0.5f;
 #endif
 
         auto thiz = makeRef(this);  // 保证线程回来之前不析构
