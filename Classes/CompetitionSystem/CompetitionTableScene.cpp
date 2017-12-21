cce0fa03f7c0c3857d9298af94fa0312d0bdc0e3
Jeff Wang <summer_insects@163.com>
2017-12-22 00:08:43 +0800

    修复越界运行错误

--- Classes/CompetitionSystem/CompetitionTableScene.cpp
+++ Classes/CompetitionSystem/CompetitionTableScene.cpp
@@ -227,7 +227,7 @@ cw::TableViewCell *CompetitionTableScene::tableCellAtIndex(cw::TableView *table,
     for (int i = 0; i < 4; ++i) {
         ptrdiff_t playerIndex = currentTable.player_indices[i];
         if (playerIndex == INVALID_INDEX) {
-            for (int k = 0; k < 5; ++k) {
+            for (int k = 0; k < 4; ++k) {
                 labels[i][k]->setString("");
             }
         }
