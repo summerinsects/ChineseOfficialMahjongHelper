b0089daad8f001ec26074f24e5221b2380dd3fea
Jeff Wang <summer_insects@163.com>
2018-01-06 10:47:36 +0800

    使追分策略对话框点击确定后不消失

--- Classes/RecordSystem/ScoreSheetScene.cpp
+++ Classes/RecordSystem/ScoreSheetScene.cpp
@@ -1361,7 +1361,7 @@ void ScoreSheetScene::onPursuitButton(cocos2d::Ref *) {
             int delta = atoi(text);
             showPursuit(delta);
         }
-        return true;
+        return false;
     }).create()->show();
 }
 
