aec9bc59dd7eb5896cb625da0108235d4bfb8df1
Jeff Wang <summer_insects@163.com>
2018-07-26 00:16:46 +0800

    改灰色的RGB（更浅）

--- Classes/UIColors.h
+++ Classes/UIColors.h
@@ -3,13 +3,13 @@
 
 #include "cocos2d.h"
 
-static const cocos2d::Color3B C3B_GRAY          ( 96,  96,  96);
+static const cocos2d::Color3B C3B_GRAY          (112, 112, 112);
 
 static const cocos2d::Color4B C4B_BLACK         ( 32,  32,  32, 255);
 static const cocos2d::Color4B C4B_RED           (254,  87, 110, 255);
 static const cocos2d::Color4B C4B_BLUE          ( 44, 121, 178, 255);
 static const cocos2d::Color4B C4B_GREEN         ( 49, 155,  28, 255);
-static const cocos2d::Color4B C4B_GRAY          ( 96,  96,  96, 255);
+static const cocos2d::Color4B C4B_GRAY          (112, 112, 112, 255);
 static const cocos2d::Color4B C4B_PURPLE        ( 89,  16,  89, 255);
 static const cocos2d::Color4B C4B_BLUE_THEME    ( 51, 204, 255, 255);
 
