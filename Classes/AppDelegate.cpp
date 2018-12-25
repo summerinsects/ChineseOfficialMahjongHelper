6cec5b41b9da9404e0382483076706a8e3b0e262
Jeff Wang <summer_insects@163.com>
2018-12-25 21:24:45 +0800

    no message

--- Classes/AppDelegate.cpp
+++ Classes/AppDelegate.cpp
@@ -39,8 +39,8 @@ AppDelegate::~AppDelegate() {
 // if you want a different context, modify the value of glContextAttrs
 // it will affect all platforms
 void AppDelegate::initGLContextAttrs() {
-    // set OpenGL context attributes: red,green,blue,alpha,depth,stencil
-    GLContextAttrs glContextAttrs = {8, 8, 8, 8, 24, 8};
+    // set OpenGL context attributes: red,green,blue,alpha,depth,stencil,multisamplesCount
+    GLContextAttrs glContextAttrs = {8, 8, 8, 8, 24, 8, 0};
 
     GLView::setGLContextAttrs(glContextAttrs);
 }
